7a335a6ef21ad55db5618ea4e45ea9aeb014f72c
Andres <a0z9@rambler.ru>
2022-10-11 17:01:38 +0300

    App basic test

---
+++ TestApp/ConsoleApp1/HumanGreets.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace andestech.learning2022.krasn.greets
+{    internal static class HumanGreets
+    {
+
+        public static void Hello() {
+            WriteLine("Hello from HumanGreets!!");
+        }
+    
+    }
+}
---
+++ TestApp/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace andestech.learning2022.krasn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 11;
+            Console.WriteLine("Hi, i'm human!");
+            Console.WriteLine("Hello, i'm human!");
+            greets.HumanGreets.Hello();
+            Console.WriteLine("Howdoyoudo!!");
+
+        }
+    }
+}
