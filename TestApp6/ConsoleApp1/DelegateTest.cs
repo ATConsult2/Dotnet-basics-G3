c8c1540f7a24a6c63c9aead791947ae59ecb803a
Andres <a0z9@rambler.ru>
2022-10-21 14:11:48 +0300

    Delegates

--- TestApp6/ConsoleApp1/DelegateTest.cs
+++ TestApp6/ConsoleApp1/DelegateTest.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
-namespace ConsoleApp1
+namespace andrestech.learning2022.krasn
 {
-    internal class Program
+    public delegate void GreetsHandler(string name); 
+    internal class DelegateTest
     {
+        static void SayHi(string name) =>  
+            WriteLine($"Hi, {name}!! ");
+        static void SayHello(string name) =>
+            WriteLine($"Hello, {name}!! ");
+        static void SaySalute(string name)
+        {
+            WriteLine($"Salute, {name}!! ");
+        }
+
+        static void SayGreets(GreetsHandler greets, string name) {
+            greets(name);
+        }
         static void Main(string[] args)
         {
+            SayGreets(SayHello, "Andrei");
+            SayGreets(SayHi, "Andrei");
+            SayGreets(SaySalute, "Andrei");
+            WriteLine($"------------------------");
+
         }
     }
 }
